e3a1027de363cf37462b5f783e4269ad156823f3
Andrey Urukov <a-urukov@dhcp-217-77-wifi.yandex.net>
2013-05-23 13:21:47 +0400

    Express.js для веб-интерфейса приложения

---
+++ web/static/js/script.js
@@ -0,0 +1,18 @@
+/* Author: YOUR NAME HERE
+ */
+
+$(document).ready(function() {
+
+    var socket = io.connect();
+
+    $('#sender').bind('click', function() {
+        socket.emit('message', 'Message Sent on ' + new Date());
+    });
+
+    socket.on('server_message', function(data) {
+        $('#receiver').append('<li>' + data + '</li>');
+    });
+
+    $('#dlg-btn').button('loading');
+
+});
