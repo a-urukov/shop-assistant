ae3ec4762ad36bb65c386a95f24567ffa5af4185
Andrey Urukov <a-urukov@dhcp172-229-red.yandex.net>
2013-06-05 18:48:59 +0400

    Новая структура проекта

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var connect = require('connect'),
 
 //Setup Express
 var server = express.createServer();
+
 server.configure(function(){
     server.set('views', __dirname + '/views');
     server.set('view options', { layout: false });
@@ -41,16 +42,19 @@ server.error(function(err, req, res, next){
         });
     }
 });
-server.listen( port);
+server.listen(port);
 
 //Setup Socket.IO
 var io = io.listen(server);
+
 io.sockets.on('connection', function(socket) {
     console.log('Client Connected');
+
     socket.on('message', function(data) {
         socket.broadcast.emit('server_message', data);
         socket.emit('server_message', data);
     });
+
     socket.on('disconnect', function() {
         console.log('Client Disconnected.');
     });
